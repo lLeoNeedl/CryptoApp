8e36b659bbda91390d53c5f143a4a00e2037b6b3
Leonid Lavrov <274852@gmail.com>
2023-01-29 12:43:39 +0200

    Adding styles.xml

--- app/src/main/java/com/demo/cryptoapp/data/workers/RefreshDataWorker.kt
+++ app/src/main/java/com/demo/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -1,27 +1,39 @@
 package com.demo.cryptoapp.data.workers
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.CoroutineWorker
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkerParameters
+import com.demo.cryptoapp.R
 import com.demo.cryptoapp.data.database.AppDatabase
 import com.demo.cryptoapp.data.mapper.CoinInfoMapper
 import com.demo.cryptoapp.data.network.ApiFactory
 import kotlinx.coroutines.delay
 
 class RefreshDataWorker(
-    context: Context,
+    private val context: Context,
     workerParameters: WorkerParameters
 ) : CoroutineWorker(context, workerParameters) {
 
     private val mapper = CoinInfoMapper()
     private val coinInfoDao = AppDatabase.getInstance(context).coinPriceInfoDao()
     private val apiService = ApiFactory.apiService
+    private val notificationManager =
+        applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
     override suspend fun doWork(): Result {
+        var isLoading = false
         while (true) {
             try {
+                if (!isLoading) {
+                    showNotification()
+                }
                 val topCoins = apiService.getTopCoinsInfo(limit = 50)
                 val fromSymbols = mapper.mapNamesListToString(topCoins)
                 val jsonContainer =
@@ -31,13 +43,37 @@ class RefreshDataWorker(
                 val dbModelList =
                     coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
                 coinInfoDao.insertPriceList(dbModelList)
+                isLoading = true
             } catch (e: Exception) {
+                notificationManager.cancel(1)
+                isLoading = false
             }
             delay(10000)
         }
     }
 
+    private fun showNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle("Background update")
+            .setContentText("The content is updating")
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        notificationManager.notify(1, notification)
+    }
+
+
     companion object {
+        private const val CHANNEL_ID = "Background update_id"
+        private const val CHANNEL_NAME = "Background update"
 
         const val NAME = "RefreshDataWorker"
 
